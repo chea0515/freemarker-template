0160f12123c365e519e709e9aacb33b1c3563fbb
chea0515 <37820899+chea0515@users.noreply.github.com>
2020-05-07 18:31:40 +0800

    Create TemplateApplication.java

---
+++ src/main/com/cc/framework/freemarker/template/TemplateApplication.java
@@ -0,0 +1,16 @@
+package com.cc.framework.freemarker.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chea0515@163.com
+ */
+@SpringBootApplication
+public class TemplateApplication {
+
+    public static void main(String[] args) {
+        // run app
+        SpringApplication.run(TemplateApplication.class, args);
+    }
+}
